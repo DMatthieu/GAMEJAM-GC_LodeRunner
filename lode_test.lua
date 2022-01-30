76cc19b65954fa04bd4c009c9e21e2591703e5d4
Madel <matt.delporte6@gmail.com>
2022-01-30 11:16:41 +0100

    Collide fct & Player SM: Walk- Stand state sys

--- lode_test.lua
+++ lode_test.lua
@@ -25,7 +25,7 @@ timer={}
 
 player={}
 	player.x=90
-	player.y=24
+	player.y=32
 	player.sprite=1
 	player.state="idle"
 	player.timer=0
@@ -39,23 +39,41 @@ player={}
 	player.vy=0
 	player.speed=1
 	player.maxSpeed=1
+	player.state="Def 1st value"
 
-	player.x1=0
-	player.y1=0
-	player.x2=0
-	player.y2=0
-	player.x3=0
-	player.y3=0
-	player.x4=0
-	player.y4=0
-	player.x5=0
-	player.y5=0
-	player.x6=0
-	player.y6=0
-	player.x7=0
-	player.y7=0
-	player.x8=0
-	player.y8=0
+	player.x1_inner=0
+	player.y1_inner=0
+	player.x2_inner=0
+	player.y2_inner=0
+	player.x3_inner=0
+	player.y3_inner=0
+	player.x4_inner=0
+	player.y4_inner=0
+	player.x5_inner=0
+	player.y5_inner=0
+	player.x6_inner=0
+	player.y6_inner=0
+	player.x7_inner=0
+	player.y7_inner=0
+	player.x8_inner=0
+	player.y8_inner=0
+
+	player.x1_outer=0
+	player.y1_outer=0
+	player.x2_outer=0
+	player.y2_outer=0
+	player.x3_outer=0
+	player.y3_outer=0
+	player.x4_outer=0
+	player.y4_outer=0
+	player.x5_outer=0
+	player.y5_outer=0
+	player.x6_outer=0
+	player.y6_outer=0
+	player.x7_outer=0
+	player.y7_outer=0
+	player.x8_outer=0
+	player.y8_outer=0
  
 vilain={}
 	vilain.x=90
@@ -73,24 +91,43 @@ vilain={}
 	vilain.vx=0
 	vilain.vy=0
 
-	vilain.x1=0
-	vilain.y1=0
-	vilain.x2=0
-	vilain.y2=0
-	vilain.x3=0
-	vilain.y3=0
-	vilain.x4=0
-	vilain.y4=0
-	vilain.x5=0
-	vilain.y5=0
-	vilain.x6=0
-	vilain.y6=0
-	vilain.x7=0
-	vilain.y7=0
-	vilain.x8=0
-	vilain.y8=0
+	vilain.x1_inner=0
+	vilain.y1_inner=0
+	vilain.x2_inner=0
+	vilain.y2_inner=0
+	vilain.x3_inner=0
+	vilain.y3_inner=0
+	vilain.x4_inner=0
+	vilain.y4_inner=0
+	vilain.x5_inner=0
+	vilain.y5_inner=0
+	vilain.x6_inner=0
+	vilain.y6_inner=0
+	vilain.x7_inner=0
+	vilain.y7_inner=0
+	vilain.x8_inner=0
+	vilain.y8_inner=0
 
-collidables = {16,5,6,7,8,19,20}
+	vilain.x1_outer=0
+	vilain.y1_outer=0
+	vilain.x2_outer=0
+	vilain.y2_outer=0
+	vilain.x3_outer=0
+	vilain.y3_outer=0
+	vilain.x4_outer=0
+	vilain.y4_outer=0
+	vilain.x5_outer=0
+	vilain.y5_outer=0
+	vilain.x6_outer=0
+	vilain.y6_outer=0
+	vilain.x7_outer=0
+	vilain.y7_outer=0
+	vilain.x8_outer=0
+	vilain.y8_outer=0
+
+collidables={}
+ collidables.wallDown={16}
+
 playerState={}
  playerState.ladder_up=false
 --
@@ -107,7 +144,7 @@ playerState={}
 
 -- Utilisée dans pour le timer affiché dans l'UI
 -- Agit sur la table 'global' timer directement.
-function time()    
+function Time()    
 	if timer.ms >= 1 then
 		timer.ms = 0
 		timer.s = timer.s + 1
@@ -136,28 +173,6 @@ end
 -- *******************************************************************	
 
 
--- Met à jour les hotspots de la hitbox d'une entité
--- ARG: Entity table au format d'un MOB (player, ennemi...)
-function update_hotspots_location(pEntity) 
-	pEntity.x1 = pEntity.x
-	pEntity.y1 = pEntity.y - 1
-	pEntity.x2 = pEntity.x + 8
-	pEntity.y2 = pEntity.y - 1
-	pEntity.x3 = pEntity.x
-	pEntity.y3 = pEntity.y + 8
-	pEntity.x4 = pEntity.x + 8
-	pEntity.y4 = pEntity.y + 8
-	pEntity.x5 = pEntity.x - 1
-	pEntity.y5 = pEntity
-	pEntity.x6 = pEntity.x - 1
-	pEntity.y6 = pEntity.y + 8
-	pEntity.x7 = pEntity.x + 8
-	pEntity.y7 = pEntity.y
-	pEntity.x8 = pEntity.x + 8
-	pEntity.y8 = pEntity.y + 8
-	
-	return pEntity
-end
 
 --[[
  █████  ███    ██ ██ ███    ███ ███████         ██     ██████  ██████   █████  ██     ██     ███████ ███    ██ ████████ ██ ████████ ██    ██ 
@@ -195,7 +210,7 @@ end
 
 -- Affiche une entité à l'écran
 -- ARG: Entity table au format d'un MOB (player, ennemi...)
-function draw_entity(pEntity)
+function Draw_entity(pEntity)
 	spr(pEntity.sprite, pEntity.x, pEntity.y, 0, 1, pEntity.revertSpr, 0)
 end
 
@@ -203,11 +218,266 @@ end
 -- Affiche une l'UI de la partie à l'écran (Timer, contours fenetre jeu etc.). 
 -- ATTENTION: Ne concerne pas un menu quelconque, ni game over, ni succès etc. 
 -- Pas d'arguments utilisés. Se base sur des Variables globales.
-function drawUi()
+function DrawUi()
 	print("Timer: "..timer.m.." : "..timer.s.." : "..(math.floor(timer.ms * 10)) * 10, timer.x, timer.y, timer.c)
+	print(" | PlayerState: "..player.state, timer.x+85, timer.y, timer.c)
 	rectb(0, 0, 240, 136, 1)
 	line(0, 8, 240, 8, 1)	
 end
+
+--[[
+ ██████  ██████  ██      ██      ██ ███████ ██  ██████  ███    ██ ███████     ██   ██ ███████ ██      ██████  ███████ ██████  ███████ 
+██      ██    ██ ██      ██      ██ ██      ██ ██    ██ ████   ██ ██          ██   ██ ██      ██      ██   ██ ██      ██   ██ ██      
+██      ██    ██ ██      ██      ██ ███████ ██ ██    ██ ██ ██  ██ ███████     ███████ █████   ██      ██████  █████   ██████  ███████ 
+██      ██    ██ ██      ██      ██      ██ ██ ██    ██ ██  ██ ██      ██     ██   ██ ██      ██      ██      ██      ██   ██      ██ 
+ ██████  ██████  ███████ ███████ ██ ███████ ██  ██████  ██   ████ ███████     ██   ██ ███████ ███████ ██      ███████ ██   ██ ███████ 
+]]
+-- *************** (TRANSVERSE au JOUEUR et AUTRES ENTITES) *******************
+-- *******************************************************************************
+
+
+-- Met à jour les hotspots interieurs de l'entité en paramètre en fonction de ses coordonées propres
+-- ARG: Entity table au format d'un MOB (player, ennemi...)
+function Uppdate_inner_hotspots_location(pEntity) 
+	pEntity.x1_inner = pEntity.x		-- hotspot haut gauche
+	pEntity.y1_inner = pEntity.y
+
+	pEntity.x2_inner = pEntity.x + 7  -- hotspot haut droit
+	pEntity.y2_inner = pEntity.y
+
+	pEntity.x3_inner = pEntity.x		-- hotspot bas gauche
+	pEntity.y3_inner = pEntity.y + 7
+
+	pEntity.x4_inner = pEntity.x + 7  -- hotspot bas droite
+	pEntity.y4_inner = pEntity.y + 7
+	
+	return pEntity
+end
+
+-- Met à jour les hotspots exterieurs de l'entité en paramètre en fonction de ses coordonées propres
+-- ARG: Entity table au format d'un MOB (player, ennemi...)
+-- ARG: "debug_on" | nil
+function Update_outer_hotspots_location(pEntity, pDebug) 
+	pEntity.x1_outer = pEntity.x	  -- hotspot haut gauche
+	pEntity.y1_outer = pEntity.y - 1
+	if pDebug == "debug_on" then pix(pEntity.x1_outer, pEntity.y1_outer, 2) end
+
+	pEntity.x2_outer = pEntity.x + 7  -- hotspot haut droit
+	pEntity.y2_outer = pEntity.y - 1
+	if pDebug == "debug_on" then pix(pEntity.x2_outer, pEntity.y2_outer, 2) end
+
+	pEntity.x3_outer = pEntity.x	  -- hotspot bas gauche
+	pEntity.y3_outer = pEntity.y + 8
+	if pDebug == "debug_on" then pix(pEntity.x3_outer, pEntity.y3_outer, 2) end
+
+	pEntity.x4_outer = pEntity.x + 7  -- hotspot bas droite
+	pEntity.y4_outer = pEntity.y + 8
+	if pDebug == "debug_on" then pix(pEntity.x4_outer, pEntity.y4_outer, 2) end
+
+	pEntity.x5_outer = pEntity.x - 1  -- hotspot gauche haut
+	pEntity.y5_outer = pEntity.y
+	if pDebug == "debug_on" then pix(pEntity.x5_outer, pEntity.y5_outer, 2) end
+
+	pEntity.x6_outer = pEntity.x - 1  -- hotspot gauche bas
+	pEntity.y6_outer = pEntity.y + 7
+	if pDebug == "debug_on" then pix(pEntity.x6_outer, pEntity.y6_outer, 2) end
+
+	pEntity.x7_outer = pEntity.x + 8  -- hotdpot droit haut
+	pEntity.y7_outer = pEntity.y
+	if pDebug == "debug_on" then pix(pEntity.x7_outer, pEntity.y7_outer, 2) end
+
+	pEntity.x8_outer = pEntity.x + 8  -- hotspot droit bas
+	pEntity.y8_outer = pEntity.y + 7
+	if pDebug == "debug_on" then pix(pEntity.x8_outer, pEntity.y8_outer, 2) end
+
+	return pEntity
+end
+
+-- Test la collision d'un hotspot de notre entité cible, et d'une collection.id unique d'un Collidable.
+-- ARG: pEntity: Entity table au format d'un MOB (player, ennemi...)
+-- ARG: pCollidableSubCollection: Collidable.identifier voulu pour le test de la collision. index de la sub-table. peut contenir une ou plusieurs tiles renseignées
+-- ARG: collideModeDetection: hotspots interieurs du sprite (bordure), hotspots exterieurs (en bordure egalement). (inner | outer)
+-- ARG: orientationSegment: segment du sprite testé (up | down | left | right)
+function Is_colliding(pEntity, pCollidableSubCollection, collideModeDetection, orientationSegment)
+	--params called for example: 
+	--player, collidables.wallDown, "outer", "down"
+
+	local collidable_col = pCollidableSubCollection
+
+	if collideModeDetection == "outer" then
+		
+		if orientationSegment == "up" then -- SEGMENT HAUT
+			--Coords haut de l'entité (player, ennemi), bordure exterieur du sprite
+			local x1 = pEntity.x1_outer--hotspot haut gauche
+			local y1 = pEntity.y1_outer
+			local x2 = pEntity.x2_outer--hotspot haut droite
+			local y2 = pEntity.y2_outer
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] or hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+
+		elseif orientationSegment == "down" then -- SEGMENT BAS
+			--Coords bas de l'entité (player, ennemi), bordure exterieur du sprite
+			local x1 = pEntity.x3_outer--hotspot bas gauche
+			local y1 = pEntity.y3_outer
+			local x2 = pEntity.x4_outer--hotspot bas droite
+			local y2 = pEntity.y4_outer
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] or hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+
+		elseif orientationSegment == "left" then -- SEGMENT GAUCHE
+			--Coords flan gauche de l'entité (player, ennemi), bordure exterieur du sprite
+			local x1 = pEntity.x5_outer--hotspot gauche haut
+			local y1 = pEntity.y5_outer
+			local x2 = pEntity.x6_outer--hotspot gauche bas
+			local y2 = pEntity.y6_outer
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] or hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		elseif orientationSegment == "right" then -- SEGMENT DROIT
+			--Coords flan droit de l'entité (player, ennemi), bordure exterieur du sprite
+			local x1 = pEntity.x7_outer--hotspot droit haut
+			local y1 = pEntity.y7_outer
+			local x2 = pEntity.x8_outer--hotspot droit bas
+			local y2 = pEntity.y8_outer
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] or hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		end
+
+	elseif collideModeDetection == "inner" then	
+		if orientationSegment == "up" then -- SEGMENT HAUT
+			--Coords haut de l'entité (player, ennemi), bordure interieure du sprite
+			local x1 = pEntity.x1_inner--hotspot haut gauche
+			local y1 = pEntity.y1_inner
+			local x2 = pEntity.x2_inner--hotspot haut droite
+			local y2 = pEntity.y2_inner
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] and hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		elseif orientationSegment == "down" then -- SEGMENT BAS
+			--Coords bas de l'entité (player, ennemi), bordure interieure du sprite
+			local x1 = pEntity.x3_inner--hotspot bas gauche 
+			local y1 = pEntity.y3_inner
+			local x2 = pEntity.x4_inner--hotspot bas droite
+			local y2 = pEntity.y4_inner
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] and hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		elseif orientationSegment == "left" then -- SEGMENT GAUCHE
+			--Coords flan gauche de l'entité (player, ennemi), bordure interieure du sprite
+			local x1 = pEntity.x1_inner--hotspot haut gauche 
+			local y1 = pEntity.y1_inner
+			local x2 = pEntity.x3_inner--hotspot bas gauche
+			local y2 = pEntity.y3_inner
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] and hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		elseif orientationSegment == "right" then -- SEGMENT DROIT
+			--Coords flan droit de l'entité (player, ennemi), bordure interieure du sprite
+			local x1 = pEntity.x2_inner--hotspot haut droit 
+			local y1 = pEntity.y2_inner
+			local x2 = pEntity.x4_inner--hotspot bas droit
+			local y2 = pEntity.y4_inner
+
+			--On converti les coordonées de la tuie du dessous du referentiel x/y (pixel) au referentiel x/y (Map Cell), 
+			--et l'on retourne l'id en map de la cellule pointée par chaque hotspot
+			local hotspot_1 = mget( (x1 / 8), (y1 / 8) )--sol bas gauche
+			local hotspot_2 = mget( (x2 / 8), (y2 / 8) )--sol bas droite
+
+			-- On verifie si la tile est le collisionable recherché...
+			for i=1,#collidable_col,1 do
+				if hotspot_1 == collidable_col[i] and hotspot_2 == collidable_col[i] then
+					eventCollision = true
+				end		
+			end
+
+			return eventCollision
+		end
+	end	
+
+	
+
+end
+
 
 
 --[[
@@ -220,7 +490,25 @@ end
 -- *************** Regles de mouvance du joueur. L'or, les ennemis, les scenes sont gérées dans une autre Finite State Machine. *******************
 -- ************************************************************************************************************************************************
 
+-- Fonction de gestion des différents états du joueur
+-- Applique 
+function Player_fsm()
 
+	--
+	Update_outer_hotspots_location(player, nil)
+
+	--si press (LEFT ou RIGHT) ET que player collisione exterieur avec un wallDown (sol)
+	if ( btn(2) or btn(3) ) and (Is_colliding(player, collidables.wallDown, "outer", "down")) then
+	--if ( btn(2) or btn(3) ) and (true) then
+		player.state="WALKING"	
+	elseif ( btn(2) == false and btn(3) == false and (Is_colliding(player, collidables.wallDown, "outer", "down")) ) then
+	--elseif ( btn(2) == false and btn(3) == false and (true) ) then
+		player.state="STANDING"
+	else
+		player.state="ERR COND"
+	end
+
+end
 
 --[[
 ████████ ██  ██████ 
@@ -237,11 +525,13 @@ function TIC()
 	cls(0)
 	map(0, 0, 240, 136, 0, 0)
 
-	draw_entity(player)
-	draw_entity(vilain)
+	Draw_entity(player)
+	--draw_entity(vilain)
 
-	drawUi()
-	time(timer)	
+	Player_fsm()
+
+	DrawUi()
+	Time()	
 	
 	
 
